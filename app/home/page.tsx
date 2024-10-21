60bd6d8689f21ce25d9be198c871f36b82b418de
reona-geshi <reona-geshi@cmc.co.jp>
2024-10-21 13:07:55 +0900

    update

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,4 +1,10 @@
-function Page(){
+import { NextResponse } from "next/server";
+
+function Page() {
+  async function POST(request: Request) {
+    // POSTリクエストの処理
+    return new NextResponse('POST request successful', { status: 200 });
+  }
   return <div>Success! You are Human right?</div>
 }
 
