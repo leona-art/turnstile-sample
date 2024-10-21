0a6e4012fa606a7020c0c5a1d443339fa9a40f1a
reona-geshi <reona-geshi@cmc.co.jp>
2024-10-21 13:02:20 +0900

    update

--- app/components/turnstile-form.tsx
+++ app/components/turnstile-form.tsx
@@ -1,28 +1,24 @@
 'use client';
 
-import { useState, useEffect } from 'react';
 import Script from 'next/script';
 
 export default function TurnstileForm() {
-    const [token, setToken] = useState<string | null>(null);
-
-    useEffect(() => {
-        const handleTurnstileSuccess = (event: any) => {
-            setToken(event.token);
-        };
-
-        window.addEventListener('turnstile-success', handleTurnstileSuccess);
-        return () => window.removeEventListener('turnstile-success', handleTurnstileSuccess);
-    }, []);
-
     return (
         <>
-            <div
-                className="cf-turnstile"
-                data-sitekey={process.env.NEXT_PUBLIC_TURNSTILE_SITEKEY}
-                style={{ margin: "20px" }}
-            />
-            <input type="hidden" name="cf-turnstile-response" value={token ?? ""} />
+            <form method="POST" action="/api/turnstile">
+                <div
+                    className="cf-turnstile"
+                    data-sitekey={process.env.NEXT_PUBLIC_TURNSTILE_SITEKEY}
+                    style={{
+                        margin: "20px"
+                    }}
+                />
+                <button
+                    type="submit"
+                    className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+                >Submit</button>
+            </form>
+
             <Script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer />
         </>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,7 @@ export default function Home() {
 
   return (
     <main>
-      <form method="POST" action="/api/turnstile">
-        <TurnstileForm />
-        <button
-          type="submit"
-          className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-        >Submit</button>
-      </form>
+      <TurnstileForm />
     </main>
   );
 }
